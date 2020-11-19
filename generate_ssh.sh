cba192574f2e7138de3e33caa785a5e44e6953d4
nana <haysato.personal@gmail.com>
2020-11-19 20:33:11 +0900

    時間も表示するよう修正

--- generate_ssh.sh
+++ generate_ssh.sh
@@ -6,9 +6,9 @@ if [ $# -lt 1 ]; then
 fi
 
 if [ "ed" = $1 ]; then
-  ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519.`date '+%Y-%m-%d'` -C "$USER-`date '+%Y-%m-%d'`"
+  ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519.`date '+%Y-%m-%d-%H:%M:%S'` -C "$USER-`date '+%Y-%m-%d'`"
 elif [ "rsa" = $1 ]; then
-  ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsa.`date '+%Y-%m-%d'` -C "$USER-`date '+%Y-%m-%d'`"
+  ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsa.`date '+%Y-%m-%d-%H:%M:%S'` -C "$USER-`date '+%Y-%m-%d'`"
 else
   echo "Usage: ./generate_ssh.sh [rsa or ed]"
   exit 1
