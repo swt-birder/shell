4d1ec91e16a4b042400cf1d277946fd8b443143c
nana <haysato.personal@gmail.com>
2020-11-19 20:23:57 +0900

    generate ssh key

---
+++ generate_ssh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "Usage: ./generate_ssh.sh [rsa or ed]"
+  exit 1
+fi
+
+if [ "ed" = $1 ]; then
+  ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519.`date '+%Y-%m-%d'` -C "$USER-`date '+%Y-%m-%d'`"
+elif [ "rsa" = $1 ]; then
+  ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsa.`date '+%Y-%m-%d'` -C "$USER-`date '+%Y-%m-%d'`"
+else
+  echo "Usage: ./generate_ssh.sh [rsa or ed]"
+  exit 1
+fi
